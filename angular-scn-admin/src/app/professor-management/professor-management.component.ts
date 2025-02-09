d2cd3ed8e5ba34a65331a2a9acc66a34f5696de9
Nubia B.V. <83447146+nubiabv@users.noreply.github.com>
2025-02-09 09:55:36 -0600

    Front-end del Administrador
    Se genera la estructra base del front-end del admin en Angular

---
+++ angular-scn-admin/src/app/professor-management/professor-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-professor-management',
+  imports: [],
+  templateUrl: './professor-management.component.html',
+  styleUrl: './professor-management.component.css'
+})
+export class ProfessorManagementComponent {
+
+}
